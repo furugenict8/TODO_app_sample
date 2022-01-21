7cb7488432ccfd068bd849fd4b43127b0354a0c3
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-21 14:11:40 +0900

    #2 環境構築終わり。Todo classのtitleとcreatedAtの取得方法をMapを使った方法に変えてみた。

---
+++ lib/todo.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Todo {
+  Todo(DocumentSnapshot doc) {
+    // doc.data()の型がObject型になっているのが原因かな。なぜ？
+    Map docMap = doc.data() as Map<String, dynamic>;
+    title = docMap!['title'];
+    createdAt = docMap!['createdAt'];
+  }
+  Map? docMap;
+  String? title;
+  DateTime? createdAt;
+}
