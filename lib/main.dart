c4c6a6c0ce8cc27a375775e78cefee4e33937e0a
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-23 13:44:11 +0900

    #3 firebaseからデータをリアルタイムでアプリに取ってくる。

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: ChangeNotifierProvider<MainModel>(
-        create: (_) => MainModel()..getTodoList(),
+        create: (_) => MainModel()..getTodoListRealtime(),
         child: Scaffold(
           appBar: AppBar(
             title: const Text('todo app sample'),
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -12,4 +12,20 @@ class MainModel extends ChangeNotifier {
     this.todoList = todoList;
     notifyListeners();
   }
+
+  //　データベースがリアルタイムで更新されたらアプリも更新される。
+  void getTodoListRealtime() {
+    // snapshotsはstream型。流れてくる。
+    final snapshot =
+        FirebaseFirestore.instance.collection('todoList').snapshots();
+    // listen()でstreamで流れてきた値を受け取って更新する。
+    snapshot.listen((snapshot) {
+      final docs = snapshot.docs;
+      final todoList = docs.map((doc) => Todo(doc)).toList();
+      this.todoList = todoList;
+      //　順番並べ替えのメソッドsort() 新しい順に変更。
+      todoList.sort((a, b) => b.createdAt!.compareTo(a.createdAt!));
+      notifyListeners();
+    });
+  }
 }
