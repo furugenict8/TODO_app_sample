7b54a082c8cf52abb591c08c6bb5513de0269b65
Ryouhei Furugen <furugenict8@gmail.com>
2022-01-26 17:14:43 +0900

    #7 Todoをcoriander appのBookを参考にリファクタ

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,15 +1,15 @@
 // Kboyさんの動画の書き方とは違うが一旦これでやってみる。
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// エンティティを作る。Firestoreのドキュメント自体を扱うためのオブジェクト
+// 今回はTodoという名前にしている。扱うフィールドを持たせる。
+// 今回はTodoのString titleと 作った時間 DateTime createdAt
 class Todo {
   Todo(DocumentSnapshot doc) {
-    // doc.data()の型がObject型になっているのが原因かな。なぜ？
-    Map docMap = doc.data() as Map<String, dynamic>;
-    title = docMap['title'];
-    Timestamp timestamp = docMap['createdAt'];
-    createdAt = timestamp.toDate();
+    title = doc['title'];
+    createdAt = doc['createdAt'].toDate();
   }
-  Map? docMap;
+  //　docMapはdoc.data()をとるための変数。オリジナル動画にはない。
   String? title;
   DateTime? createdAt;
 }
