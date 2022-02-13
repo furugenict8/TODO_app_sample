c4776434b5bce0373ecd52a02dc60baa1385b3f6
Ryouhei Furugen <furugenict8@gmail.com>
2022-02-13 13:45:46 +0900

    #9 slidable_sample.dart追加。flutter_slidableを自分で実装してみた。

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:todoapp/main_model.dart';
+import 'package:todoapp/todo.dart';
 
 import 'add/add_page.dart';
 
@@ -42,9 +44,7 @@ class MainPage extends StatelessWidget {
           return ListView(
               children: todoList
                   .map(
-                    (todo) => ListTile(
-                      title: Text(todo.title!),
-                    ),
+                    (todo) => SlidableListTile(todo),
                   )
                   .toList());
         }),
@@ -69,3 +69,82 @@ class MainPage extends StatelessWidget {
     );
   }
 }
+
+// slidableなListTileを作る。
+class SlidableListTile extends StatelessWidget {
+  const SlidableListTile(this.todo, {Key? key}) : super(key: key);
+
+  // 引数にTodoを持たせる。
+  final Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slidable(
+      // Specify a key if the Slidable is dismissible.
+      key: const ValueKey(0),
+
+      // The start action pane is the one at the left or the top side.
+      // 左側または上側にあるのがスタートアクションペインです。
+      startActionPane: ActionPane(
+        // A motion is a widget used to control how the pane animates.
+        // モーションは、ペインのアニメーションを制御するためのウィジェットです。
+        motion: const ScrollMotion(),
+
+        // A pane can dismiss the Slidable.
+        dismissible: DismissiblePane(onDismissed: () {}),
+
+        // All actions are defined in the children parameter.
+        children: const [
+          // A SlidableAction can have an icon and/or a label.
+          SlidableAction(
+            // 何かやる。
+            onPressed: doSomething,
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+          SlidableAction(
+            //　何かやる。
+            onPressed: doSomething,
+            backgroundColor: Color(0xFF21B7CA),
+            foregroundColor: Colors.white,
+            icon: Icons.share,
+            label: 'Share',
+          ),
+        ],
+      ),
+
+      // The end action pane is the one at the right or the bottom side.
+      endActionPane: const ActionPane(
+        motion: ScrollMotion(),
+        children: [
+          SlidableAction(
+            // An action can be bigger than the others.
+            flex: 2,
+            onPressed: doSomething,
+            backgroundColor: Color(0xFF7BC043),
+            foregroundColor: Colors.white,
+            icon: Icons.archive,
+            label: 'Archive',
+          ),
+          SlidableAction(
+            onPressed: doSomething,
+            backgroundColor: Color(0xFF0392CF),
+            foregroundColor: Colors.white,
+            icon: Icons.save,
+            label: 'Save',
+          ),
+        ],
+      ),
+
+      // The child of the Slidable is what the user sees when the
+      // component is not dragged.
+      child: ListTile(
+        title: Text(todo.title!),
+      ),
+    );
+  }
+}
+
+void doSomething(BuildContext context) {}
